de39e6ed68898f4f75519e70f5bc65fe5e677a90
Evgeny Bykovskikh <redzumi.tao@gmail.com>
2021-10-21 18:46:44 +0300

    Edit note

--- src/features/notes/LastNote.tsx
+++ src/features/notes/LastNote.tsx
@@ -1,10 +1,33 @@
 import React from 'react';
-import { gql, useSubscription } from '@apollo/client';
-import { Card } from 'antd';
+import { gql, useMutation, useSubscription } from '@apollo/client';
+import { Button, Card, Form, Input, message, Spin } from 'antd';
+import { useForm } from 'antd/lib/form/Form';
+import FormItem from 'antd/lib/form/FormItem';
+import { Note } from './notesSlice';
+
+const GET_NOTES = gql`
+  query GetNotes {
+    notes {
+      id
+      title
+    }
+  }
+`;
 
 const NOTES_SUBSCRIPTION = gql`
   subscription noteAdded {
     noteAdded {
+      id
+      title
+      body
+    }
+  }
+`;
+
+const EDIT_NOTE = gql`
+  mutation editNote($id: String!, $title: String!, $body: String!) {
+    editNote(id: $id, title: $title, body: $body) {
+      id
       title
       body
     }
@@ -12,16 +35,40 @@ const NOTES_SUBSCRIPTION = gql`
 `;
 
 const LastNote = () => {
+  const [editNote, { loading }] = useMutation(EDIT_NOTE, {
+    refetchQueries: [{ query: GET_NOTES }],
+  });
+
   const { data } = useSubscription(NOTES_SUBSCRIPTION, {
     variables: {},
   });
 
-  const note = data?.noteAdded;
+  const currentNote = data?.noteAdded;
 
-  if (note) {
+  const [form] = useForm<Note>();
+
+  const handleSubmit = () => {
+    const note = form.getFieldsValue();
+    editNote({ variables: { id: currentNote.id, ...note } }).catch((error) =>
+      message.error(error.message)
+    );
+  };
+
+  if (currentNote) {
     return (
-      <Card key={note.title} title={note.title}>
-        {note.body}
+      <Card key={currentNote.title} title={currentNote.title}>
+        {currentNote.body}
+        <Spin spinning={loading}>
+          <Form name="form" form={form}>
+            <FormItem name="title" label="Title for note">
+              <Input />
+            </FormItem>
+            <FormItem name="body" label="Body for note">
+              <Input.TextArea />
+            </FormItem>
+            <Button onClick={handleSubmit}>Add new note</Button>
+          </Form>
+        </Spin>
       </Card>
     );
   }
